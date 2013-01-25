4f6e382173096ef9abda6737e89b8ad9da60af0b
Michael Heiniger <michael.heiniger@nzz.ch>
2013-01-25 09:22:55 +0100

    added php5-dev for building gmagick, added redis-server

--- installDevEnvironmentApache.sh
+++ installDevEnvironmentApache.sh
@@ -6,18 +6,17 @@ git config --global color.diff auto
 git config --global color.status auto
 sudo apt-get install memcached
 sudo apt-get install php5-memcache
+
 # Apache and related
 sudo apt-get install apache2
 sudo a2enmod rewrite
 sudo apt-get install libapache2-mod-php5
-# Sql
-sudo apt-get install mysql-server
-sudo apt-get install php5-mysql
-sudo apt-get install sqlite3 php5-sqlite
+
+# php
+sudo apt-get install php5-cli php5-common php-apc php-pear php5-xdebug php5-curl php5 php5-dev
 sudo apt-get install graphicsmagick libgraphicsmagick1-dev
 sudo pecl install gmagick-beta
 sudo apt-get install curl
-sudo apt-get install php5-cli php5-common php-apc php-pear php5-xdebug php5-curl php5
 sudo apt-get install php5-xsl
 sudo apt-get install php5-intl
 sudo pear channel-update PEAR
@@ -25,8 +24,9 @@ sudo pear upgrade PEAR
 sudo pear channel-discover pear.phing.info
 sudo pear install phing/phing
 sudo pear install PHP_CodeSniffer
-sudo apt-get remove phpunit
+
 #install newest version of phpunit
+sudo apt-get remove phpunit
 sudo pear channel-discover pear.phpunit.de
 sudo pear channel-discover pear.symfony-project.com
 sudo pear channel-discover components.ez.no
@@ -34,11 +34,20 @@ sudo pear update-channels
 sudo pear upgrade-all
 sudo pear install --alldeps phpunit/PHPUnit
 sudo pear install --force --alldeps phpunit/PHPUnit
+
 #phpunit Skeleton Generator
 sudo pear config-set auto_discover 1
 sudo pear install pear.phpunit.de/PHPUnit_SkeletonGenerator
 sudo wget http://cs.sensiolabs.org/get/php-cs-fixer.phar -O /usr/local/bin/php-cs-fixer
 sudo chmod a+x /usr/local/bin/php-cs-fixer
+
+# Database
+sudo apt-get install mysql-server
+sudo apt-get install php5-mysql
+sudo apt-get install sqlite3 php5-sqlite
+sudo apt-get install redis-server
+
+#others
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java7-installer
